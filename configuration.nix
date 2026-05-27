8c55fdc5345e6be29f3c4ee9b6f9307eb9807806
Adam Dinwoodie <adam@dinwoodie.org>
2026-05-27 17:26:07 +0100

    Test config for systemd timer offset behaviour

--- configuration.nix
+++ configuration.nix
@@ -21,4 +21,20 @@
     "nix-command"
     "flakes"
   ];
+
+  systemd.timers.test = {
+    timerConfig = {
+      OnCalendar = "*:*:00";
+      AccuracySec = "1s";
+      RandomizedOffsetSec = "60s";
+      Persistent = true;
+    };
+    wantedBy = [ "timers.target" ];
+  };
+  systemd.services.test.serviceConfig = {
+    Type = "oneshot";
+    ExecStart = "/run/current-system/sw/bin/echo tick";
+  };
+
+  programs.bash.loginShellInit = "systemctl status test.timer";
 }
